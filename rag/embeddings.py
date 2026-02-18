cfe4a5d305bf563af2d62f0081f98ca593fec481
Manish <manishmohapatra33@gmail.com>
2026-02-18 17:01:48 +0530

    Fix memory issue using HF endpoint embeddings

--- rag/embeddings.py
+++ rag/embeddings.py
@@ -1,7 +1,14 @@
-from langchain_huggingface import HuggingFaceEmbeddings
+import os
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
+from dotenv import load_dotenv
 
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
+load_dotenv()
+
+HUGGINGFACEHUB_ACCESS_TOKEN = os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
+
+embeddings = HuggingFaceEndpointEmbeddings(
+    model="sentence-transformers/all-MiniLM-L6-v2",
+    huggingfacehub_api_token=HUGGINGFACEHUB_ACCESS_TOKEN
 )
 
 def get_embeddings():
