7f5e67424418860106578328e38b05b42611553f
Manish <manishmohapatra33@gmail.com>
2026-02-18 17:57:12 +0530

    Fix memory issue using HF inference embeddings

--- rag/embeddings.py
+++ rag/embeddings.py
@@ -1,15 +1,12 @@
 import os
-from langchain_huggingface import HuggingFaceEndpointEmbeddings
-from dotenv import load_dotenv
+from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
+
+from dotenv import load_dotenv 
 
 load_dotenv()
 
-HUGGINGFACEHUB_ACCESS_TOKEN = os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
-
-embeddings = HuggingFaceEndpointEmbeddings(
-    model="sentence-transformers/all-MiniLM-L6-v2",
-    huggingfacehub_api_token=HUGGINGFACEHUB_ACCESS_TOKEN
-)
-
 def get_embeddings():
-    return embeddings
+    return HuggingFaceInferenceAPIEmbeddings(
+        api_key=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN"),
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
